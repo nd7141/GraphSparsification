6e991c769eba9b82860ece40e83280996be319a4
Sergey Ivanov <ivanovserg990@gmail.com>
2015-07-01 19:45:53 +0300

    Add binary search phase

--- IM/Reverse/main.cpp
+++ IM/Reverse/main.cpp
@@ -1,44 +1,15 @@
 #include <iostream>
 #include <fstream>                  // for reading files
-#include <utility>                   // for std::pair
 #include <algorithm>                 // for std::for_each
-#include <sys/time.h>
 #include "dirent.h"                 // for reading files from directory
 #include <queue>                    // std::priority_queue
-#include <functional>               // std::greater
-#include <sys/stat.h>               // to check for files
-#include <string>
-#include <sstream>
-#include <vector>
-
-#include <boost/graph/graph_traits.hpp>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/property_map/property_map.hpp>
-#include <boost/graph/graph_concepts.hpp>
-#include <boost/graph/named_function_params.hpp>
-#include <boost/pending/disjoint_sets.hpp>
-#include <boost/pending/indirect_cmp.hpp>
-#include <boost/graph/random.hpp>
-#include <boost/random.hpp>
-#include <boost/functional/hash.hpp>
-#include "boost/lexical_cast.hpp"
-#include <boost/lexical_cast.hpp>
+#include <map>
+#include <iomanip>
 #include <boost/tokenizer.hpp>
-#include <boost/graph/dijkstra_shortest_paths.hpp>
-#include <boost/graph/connected_components.hpp>
-//#include "boost/filesystem/operations.hpp"
-//#include "boost/filesystem/path.hpp"
-//#include "boost/progress.hpp"
+#include <boost/lexical_cast.hpp>
 
-using namespace boost;
+//using namespace boost;
 using namespace std;
-typedef property < edge_weight_t, double >Weight;
-typedef adjacency_list < vecS, vecS, undirectedS, no_property, Weight > WeightedGraph;
-typedef adjacency_list < vecS, vecS, undirectedS> DeterministicGraph;
-typedef typename graph_traits<DeterministicGraph>::vertex_iterator vertex_iterator;
-typedef typename graph_traits<DeterministicGraph>::vertex_descriptor Vertex;
-typedef typename graph_traits<WeightedGraph>::edge_descriptor Edge;
-typedef typename property_map < WeightedGraph, edge_weight_t >::type myWeight;
 
 struct CC {
     int idx;
@@ -71,7 +42,6 @@ vector<vector<int> > find_top_CC(vector<vector<int> >& PW, int V, int T) {
     for (int i = 0; i < V; ++i)
         explored[i] = false;
     int cc_number = 0;
-    int unit_sized = 0;
 
     for (int i = 0; i < V; ++i) {
         if (!explored[i]) {
@@ -128,46 +98,24 @@ bool buildPW(vector<vector<int> >& PW, string dataset) {
     return true;
 }
 
-// read Graph with probabilities from file
-void buildGraph (WeightedGraph& G, string input){
-
-    typedef typename WeightedGraph::edge_property_type Weight;
-    typename graph_traits<WeightedGraph>::edge_iterator ei, eiend;
-
-    map<int, int> m;
-
-    ifstream infile(input.c_str());
+void readGraph(vector<vector<pair<int, double> > >& G, string dataset_f) {
+    ifstream infile(dataset_f.c_str());
     if (infile==NULL){
         cout << "Unable to open the input file\n";
     }
-    int u, v, u_, v_;
+    int u, v;
     double p;
-    Edge e;
-    bool inserted;
-    int mapped=0;
-    int edge_count=0;
-
+    pair<int, double> endpoint1, endpoint2;
+    vector<int> a;
     while (infile >> u >> v >> p){
-        if (m.find(u)== m.end()){
-            m[u]=mapped;
-            mapped++;
-        }
-        if (m.find(v)==m.end()){
-            m[v]=mapped;
-            mapped++;
-        }
-        u_=m[u]; v_=m[v];
-        tie(e,inserted)=add_edge(u_, v_, Weight(p), G);
-        if (!inserted) {
-            cout << "Unable to insert edge\n";
-        }
-        edge_count++;
+        endpoint1 = make_pair(u, p);
+        endpoint2 = make_pair(v, p);
+        G[v].push_back(endpoint1);
+        G[u].push_back(endpoint2);
     }
-    cout <<"E = "<< edge_count << " V = "<< mapped << endl;
 }
 
 int accumulate_scores(map<int, double>& scores, string dataset, int T, int V, int R) {
-    cout << "In the accumulate function" << endl;
 
     float amount = 0;
     for (int i=0; i<R; i++) {
@@ -186,18 +134,14 @@ int accumulate_scores(map<int, double>& scores, string dataset, int T, int V, in
         amount += (float)topT_CC.size()/(float)R;
     }
     int L = ceil(amount);
-    cout << "L:" << L << endl;
-    cout << "Finished accumulation phase" << endl;
 
     return L;
 }
 
-void select_initial_nodes(vector<int>& S, map<int, double>& scores, int L, WeightedGraph G) {
-    cout << "Selecting initial nodes..." << endl;
+void select_nodes(vector<int>& S, map<int, double>& scores, map<int, bool>& selected, int L, vector<vector<pair<int, double> > > G) {
 
     int argmax_v;
     double max_score;
-    map<int, bool> selected;
     for (int i=0; i<L; ++i) {
         argmax_v = -1;
         max_score = 0;
@@ -216,42 +160,129 @@ void select_initial_nodes(vector<int>& S, map<int, double>& scores, int L, Weigh
         else {
             S.push_back(argmax_v);
             selected[argmax_v] = true;
+            for (vector<pair<int, double> >::iterator it=G[argmax_v].begin(); it!=G[argmax_v].end(); ++it) {
+                int u = it->first;
+                double p = it->second;
+                if (!selected[u]) {
+                    scores[u] *= (1-p);
+                }
+            }
         }
     }
-    cout << "Selected nodes: ";
-    for (int i; i<S.size(); i++) {
+}
+
+double calculateSpread(vector<vector<pair<int, double> > > G, vector<int> S, int V, int I) {
+    double cascade_size = 0;
+    int iter = 0;
+    map<int, bool> activated;
+    vector<int> T;
+    while (iter < I) {
+        // activate seeds
+        for (int i = 0; i < V; ++i) {
+            activated[i] = false;
+        }
+        for (vector<int>::iterator it = S.begin(); it != S.end(); ++it) {
+            activated[*it] = true;
+            T.push_back(*it);
+        }
+        // activate new nodes
+        vector<int>::size_type ix = 0; // activated node index
+        int u;
+        while (ix < T.size()) {
+            u = T[ix];
+            for (vector<pair<int, double> >::iterator it = G[u].begin(); it != G[u].end(); ++it) {
+                int v = it->first;
+                double p = it->second;
+                if (!activated[v]) {
+                    double random = (double) rand()/RAND_MAX;
+                    if (random <= p) {
+                        activated[v] = true;
+                        T.push_back(v);
+                    }
+                }
+            }
+            ix++;
+        }
+        cascade_size += T.size();
+        iter++;
+        T.clear();
+    }
+    double spread = (double)cascade_size/(double)I;
+    return spread;
+}
+
+void add_more_nodes(vector<int>& S, int T, vector<vector<pair<int, double> > > G, map<int, double> scores, int L, int V, int I, map<int, bool>& selected) {
+    // calculate spread of selected initial nodes
+    double sigma = calculateSpread(G, S, V, I);
+    int High=L;
+    while (sigma < T) {
+        select_nodes(S, scores, selected, High, G);
+        High *= 2;
+        sigma = calculateSpread(G, S, V, I);
+    }
+    int Low = floor(High/2);
+    int k;
+    vector<int> resultS;
+    while (Low+1 < High) {
+        k = Low + floor((High - Low)/2);
+        resultS = vector<int> (&S[0], &S[k]);
+        sigma = calculateSpread(G, resultS, V, I);
+        if (sigma >= T)
+            High = k;
+        else
+            Low = k;
+    }
+    if (sigma >=T)
+        S = vector<int> (&S[0], &S[Low]);
+    else
+        S = vector<int> (&S[0], &S[High]);
+    cout << "Final nodes: ";
+    for (int i=0; i<S.size(); i++) {
         cout << S[i] << " ";
     }
+    sigma = calculateSpread(G, resultS, V, I);
+    cout << "Spread: " << sigma << " Wanted spread:" << T << endl;
 }
-//TODO write calculation spread nad binary search
 
 int main(int argc, char* argv[]) {
+    srand(time(NULL));
 
     // read parameters from command-line
     const string dataset = argv[1]; // filename of the dataset
     const long int V = atoi(argv[2]); // number of nodes
     const int R = atoi(argv[3]); // number of PWs
     const int T = atoi(argv[4]);         // threshold of active nodes
-    const string outfilename = argv[5]; // filename of the seeds
+    const int I = atoi(argv[5]);   // number of MC calculations
+    const string outfilename = argv[6]; // filename of the seeds
 
     cout << "Started processing..." <<endl;
 
     cout << "Graph: " << dataset << " T: " << T << endl;
 
     // read graph from the file
-    WeightedGraph G(V);
-    buildGraph(G, dataset);
+    cout << "Reading graph form the file..." << endl;
+    vector<vector<pair<int, double> > > G(V);
+    readGraph(G, dataset);
 
     //    accumulate scores
+    cout << "Accumulating scores..." << endl;
     map<int, double> scores;
     int L = accumulate_scores(scores, dataset, T, V, R);
 
     //    select initial nodes
+    cout << "Selecting initial nodes..." << endl;
     vector<int> S;
-    select_initial_nodes(S, scores, L, G);
-//
-//    //    add more nodes
-//    add_more_nodes(S, T, G, scores);
+    map<int, bool> selected;
+    select_nodes(S, scores, selected, L, G);
+
+    //    add more nodes
+    cout << "Adding the rest nodes..." << endl;
+    add_more_nodes(S, T, G, scores, L, V, I, selected);
+
+    //    writing to file seed sizes
+    ofstream result_file(outfilename, ios_base::app);
+    result_file << T << " " << S.size() << endl;
+    result_file.close();
 
     cout << "Finished code" << endl;
     return 0;
